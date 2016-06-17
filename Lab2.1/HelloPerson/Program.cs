a710d78efa1c6732c79982ac78a191bf2fa0524c
KamilKhair <KamilK@code-college.net>
2016-06-18 00:16:09 +0300

    Lab 2.1: Basic Console I/O

---
+++ Lab2.1/HelloPerson/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HelloPerson
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("hello " + name + "!");
+            Console.WriteLine("Please enter an integer number in the range 1-10");
+            int number;
+            label1:
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Error: Please enter only an integer number");
+            }
+            if (number > 10 || number < 1)
+            {
+                Console.WriteLine("Error: Please enter an integer number in the range 1-10");
+                goto label1;
+            }
+            for (var i = 0; i < number; ++i)
+            {
+                Console.WriteLine(AddSpaces(i) + name);
+            }
+        }
+
+        private static string AddSpaces(int i)
+        {
+            var spaces = "";
+            for (var j = 0; j < i; ++j)
+            {
+                spaces += " ";
+            }
+            return spaces;
+        }
+    }
+}
