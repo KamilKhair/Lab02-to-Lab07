10d05d1ca73f4f7d7e1a74f95bd93458de4f4cbf
KamilKhair <KamilK@code-college.net>
2016-06-18 19:50:54 +0300

    Using Loops

---
+++ Lab2.5/MulBoard/MulBoard/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MulBoard
+{
+    internal class Program
+    {
+        static int[,] _myArr = new int[10, 10];
+
+        public static void Main()
+        {
+            for (var i = 0; i < 10; ++i)
+            {
+                Display(i);
+            }
+        }
+
+        public static void Display(int i)
+        {
+            for (var j = 0; j < 10; ++j)
+            {
+                _myArr[i, j] = (i + 1) * (j + 1);
+                Console.Write($"{_myArr[i, j],4}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
