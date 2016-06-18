ba7b98d025247cebe0e06205b33541198e42d1fb
KamilKhair <KamilK@code-college.net>
2016-06-18 20:41:07 +0300

    Creating Reference Types

---
+++ Lab3.1/Accounts/AccountsLib/Class1.cs
@@ -0,0 +1,86 @@
+﻿namespace AccountsLib
+{
+    public class Account
+    {
+        readonly int _id;
+        double _balance;
+        internal Account(int id)
+        {
+            _id = id;
+        }
+
+        public int ID
+        {
+            get { return _id; }
+        }
+
+        public double Balance
+        {
+            get { return _balance; }
+        }
+
+        public bool Deposit(double amount)
+        {
+            try
+            {
+                if (amount > 0)
+                {
+                    _balance += amount;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (_balance > amount)
+            {
+                _balance -= amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transfer(Account distenationAcc, double amount)
+        {
+            try
+            {
+                if (_balance >= amount)
+                {
+                    distenationAcc._balance += amount;
+                    _balance -= amount;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+    public static class AccountFactory
+    {
+        public static Account CreateAccount(int id, double balance)
+        {
+            Account account = new Account(id);
+            account.Deposit(balance);
+            return account;
+        }
+    }
+}
---
+++ Lab3.1/Accounts/Bank/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using AccountsLib;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Let's create our first account! Please enter Account ID:");
+            int id;
+            if (!int.TryParse(Console.ReadLine(), out id))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            Console.WriteLine("Please enter initial balance:");
+            double balance;
+            if (!double.TryParse(Console.ReadLine(), out balance))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            Account acc = AccountFactory.CreateAccount(id, balance);
+            Console.WriteLine("Account id = " + id + " successfully created!");
+
+            Console.WriteLine("Let's Deposit some money to our account!, please enter amount:");
+            double deposit;
+            if (!double.TryParse(Console.ReadLine(), out deposit))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            if (acc.Deposit(deposit))
+            {
+                Console.WriteLine("Account id = " + id + " has a new balance = " + acc.Balance);
+            }
+            else
+            {
+                Console.WriteLine("Hey! you can not deposit 0 or negative amount! ");
+                return;
+            }
+
+            Console.WriteLine("Let's Withdraw some money from our account!, please enter amount:");
+            double withdraw;
+            if (!double.TryParse(Console.ReadLine(), out withdraw))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            if (acc.Withdraw(withdraw))
+            {
+                Console.WriteLine("Account id = " + id + " has a new balance = " + acc.Balance);
+            }
+            else
+            {
+                Console.WriteLine("Hey! you can not go into overdraft! ");
+                return;
+            }
+
+            Console.WriteLine("Let's creat our second account! Please enter Account ID:");
+            int id2;
+            if (!int.TryParse(Console.ReadLine(), out id2))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            if (id2 == id)
+            {
+                Console.WriteLine("Error, you can not create an account with the same id !");
+                return;
+            }
+            Console.WriteLine("Please enter initial balance:");
+            double balance2;
+            if (!double.TryParse(Console.ReadLine(), out balance2))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            Account acc2 = AccountFactory.CreateAccount(id2, balance2);
+            Console.WriteLine("Account id = " + id2 + " successfully created!");
+
+            Console.WriteLine("Let's Transfer some money from our first accout to second account, please enter amount:");
+            double amount;
+            if (!double.TryParse(Console.ReadLine(), out amount))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            if (acc.Transfer(acc2, amount))
+            {
+                Console.WriteLine("Account id = " + id2 + " has a new balance = " + acc2.Balance);
+                Console.WriteLine("Account id = " + id + " has a new balance = " + acc.Balance);
+            }
+            else
+            {
+                Console.WriteLine("Error, wrong amount");
+                return;
+            }
+
+            Console.WriteLine("Thank you very much ! See you later :)");
+        }
+    }
+}
