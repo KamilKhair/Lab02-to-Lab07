c6d8d90e13a7a9a0d50d552cbee3af367cd5b117
KamilKhair <KamilK@code-college.net>
2016-06-18 20:28:09 +0300

    Binary Displays

---
+++ Lab2.6/BinaryDisplay/BinaryDisplay/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace BinaryDisplay
+{
+    public class BinaryNum
+    {
+        public BinaryNum(int num)
+        {
+            _num = num;
+        }
+
+        int _num;
+
+        public string BinaryNumber { get; private set; }
+
+        public void ConvertToBinary()
+        {
+            BinaryNumber = Convert.ToString(_num, 2); // no boxing ! for more information read this article: https://selvasamuel.wordpress.com/2008/03/14/boxingunboxing-in-net/
+        }
+
+        public int NumberOfOnes()
+        {
+            if (BinaryNumber == "")
+            {
+                return 0;
+            }
+            var numberOfOnes = 0;
+            for (var i = 0; i < BinaryNumber.Length; ++i)
+            {
+                if ((_num & 1) == 1)
+                {
+                    ++numberOfOnes;
+                }
+                _num >>= 1;
+            }
+            return numberOfOnes;
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            //2.6.2
+            Console.WriteLine("Please enter an integer number ");
+            int num;
+            while (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("Please enter an integer number ");
+            }
+
+            BinaryNum number = new BinaryNum(num);
+            number.ConvertToBinary();
+            Console.WriteLine($"Binary representation of {num} is {number.BinaryNumber}");
+
+            //2.6.3
+            Console.WriteLine($"There are {number.NumberOfOnes()} \"1\"s");
+        }
+    }
+}
---
+++ Lab2.6/BinaryDisplay/BinaryDisplayUnitTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using BinaryDisplay;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryDisplayUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_ConvertToBinary_Positive()
+        {
+            var binary = new BinaryNum(5); 
+            binary.ConvertToBinary();
+            Assert.AreEqual("101", binary.BinaryNumber);
+        }
+
+        [TestMethod]
+        public void Test_ConvertToBinary_Negative()
+        {
+            var binary = new BinaryNum(-5);
+            binary.ConvertToBinary();
+            Assert.AreEqual("11111111111111111111111111111011", binary.BinaryNumber);
+        }
+
+        [TestMethod]
+        public void Test_NumberOfOnes_Negative()
+        {
+            var binary = new BinaryNum(-5);
+            binary.ConvertToBinary();
+            Assert.AreEqual(31, binary.NumberOfOnes());
+        }
+
+        [TestMethod]
+        public void Test_NumberOfOnes_Positive()
+        {
+            var binary = new BinaryNum(5);
+            binary.ConvertToBinary();
+            Assert.AreEqual(2, binary.NumberOfOnes());
+        }
+    }
+}
