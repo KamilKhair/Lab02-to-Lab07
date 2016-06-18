7b74aae083e7be664d1e174e949902b16e4a47c0
KamilKhair <KamilK@code-college.net>
2016-06-18 22:26:25 +0300

    Inheritance & Polymorphism

---
+++ Lab5.1/Shapes/ShapeLib/Class1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Globalization;
+
+namespace ShapeLib
+{
+    public abstract class Shape
+    {
+        protected Shape()
+        {
+            Color = ConsoleColor.White;
+        }
+        protected Shape(ConsoleColor color)
+        {
+            Color = color;
+        }
+
+        private ConsoleColor Color { get; }
+
+        public abstract double Area { get; }
+
+        public virtual void Display()
+        {
+            Console.BackgroundColor = Color;
+            Console.WriteLine("I'm a " + ToString());
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public Rectangle(double width, double hieght)
+        {
+            Width = width;
+            Hieght = hieght;
+            Area = hieght * Width * 0.5;
+        }
+
+        public Rectangle(double width, double hieght, ConsoleColor c) : base(c)
+        {
+            Width = width;
+            Hieght = hieght;
+            Area = hieght * Width * 0.5;
+        }
+
+        public override double Area { get; }
+
+        public double Width { get; }
+        public double Hieght { get; }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Width = " + Width.ToString(CultureInfo.CurrentCulture) + " Hieght = " + Hieght.ToString(CultureInfo.CurrentCulture));
+        }
+    }
+
+    public class Ellipse : Shape
+    {
+        public Ellipse(double radius1, double radius2)
+        {
+            Radius1 = radius1;
+            Radius2 = radius2;
+            Area = Math.PI * Radius1 * Radius2;
+        }
+
+        public Ellipse(double radius1, double radius2, ConsoleColor c) : base(c)
+        {
+            Radius1 = radius1;
+            Radius2 = radius2;
+            Area = Math.PI * Radius1 * Radius2;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Radius1 = " + Radius1.ToString(CultureInfo.CurrentCulture) + " Radius2 = " + Radius2.ToString(CultureInfo.CurrentCulture));
+        }
+
+        public override double Area { get; }
+
+        protected double Radius1 { get; }
+        protected double Radius2 { get; }
+    }
+
+    public class Circle : Ellipse
+    {
+        public Circle(double radius) : base(radius, radius)
+        {
+            Radius = radius;
+        }
+        public Circle(double radius, ConsoleColor c) : base(radius, radius, c)
+        {
+            Radius = radius;
+        }
+        protected double Radius { get; }
+    }
+}
---
+++ Lab5.1/Shapes/ShapesApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using ShapeLib;
+
+namespace ShapesApp
+{
+    internal class ShapeManager
+    {
+        public ShapeManager()
+        {
+            Shapes = new List<Shape>();
+        }
+
+        public List<Shape> Shapes { get; }
+
+        public Shape this[int i] => Shapes[i];
+
+        private int Count => Shapes.Count;
+
+        public void Add(Shape shape)
+        {
+            Shapes.Add(shape);
+        }
+
+        public void DisplayAll()
+        {
+            //foreach (var sh in Shapes)
+            //{
+
+            //    sh.Display();
+            //    Console.WriteLine("The area is: " + sh.Area);
+            //}
+
+            for (int i = 0; i < Count; ++i)
+            {
+                Shapes[i].Display();
+                Console.WriteLine("The area is: " + Shapes[i].Area);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            ShapeManager shm = new ShapeManager();
+            shm.Add(new Rectangle(5, 4));
+            shm.Add(new Ellipse(5, 10, ConsoleColor.Cyan));
+            shm.Add(new Circle(5, ConsoleColor.Yellow));
+
+            shm.DisplayAll();
+        }
+    }
+}
