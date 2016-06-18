8cc20b0101f9519dbdfdb62bce617a98af8f2513
KamilKhair <KamilK@code-college.net>
2016-06-18 19:41:14 +0300

    Command Line Arguments - Quad

---
+++ Lab2.4/Quad/Quad/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Quad
+{
+    public class Quad
+    {
+        public Quad(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+            X1 = double.NaN;
+            X2 = double.NaN;
+        }
+
+        public double A { get; }
+        public double B { get; }
+        public double C { get; }
+        public double X1 { get; private set; }
+        public double X2 { get; private set; }
+
+        public void Calculate()
+        {
+            var sqrtPart = B * B - 4 * A * C;
+            if (A == 0 && B != 0)
+            {
+                X1 = -1.0*C/B;
+            }
+            else if (A == 0)
+            {
+                // do nothing
+            }
+            else if (sqrtPart > 0)
+            {
+                X1 = (-B + Math.Sqrt(sqrtPart)) / (2 * A);
+                X2 = (-B - Math.Sqrt(sqrtPart)) / (2 * A);
+            }
+            else if (sqrtPart == 0 && (A != 0 || B != 0 || C != 0))
+            {
+                X1 = (-B + Math.Sqrt(sqrtPart)) / (2 * A);
+            }
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Command Line arguments error!");
+                return;
+            }
+            double a, b, c;
+            if (!double.TryParse(args[0], out a))
+            {
+                Console.WriteLine("Unable to parse " + args[0]);
+                return;
+            }
+            if (!double.TryParse(args[1], out b))
+            {
+                Console.WriteLine("Unable to parse " + args[1]);
+                return;
+            }
+            if (!double.TryParse(args[2], out c))
+            {
+                Console.WriteLine("Unable to parse " + args[2]);
+                return;
+            }
+            var q = new Quad(a,b,c);
+            q.Calculate();
+            if (q.X1 == double.NaN && q.X2 == double.NaN)
+            {
+                Console.WriteLine("No Real solutions!");
+            }
+            else if (q.X2 == double.NaN)
+            {
+                Console.WriteLine("One Real solution: " + q.X1);
+            }
+            else
+            {
+                Console.WriteLine("Two Real solutions: " + q.X1 + " " + q.X2);
+            }
+        }
+    }
+}
---
+++ Lab2.4/Quad/QuadUnitTest/UnitTest1.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace QuadUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Two_Solutions()
+        {
+            const double a = 1;
+            const double b = 5;
+            const double c = 3;
+            var q = new Quad.Quad(a, b, c);
+            q.Calculate();
+            Assert.AreEqual(-0.6972243622680054, q.X1);
+            Assert.AreEqual(-4.302775637731995, q.X2);
+        }
+
+        [TestMethod]
+        public void Test_One_Solution_A_NotEqual_To_Zero()
+        {
+            const double a = 1;
+            const double b = 4;
+            const double c = 4;
+            var q = new Quad.Quad(a, b, c);
+            q.Calculate();
+            Assert.AreEqual(-2.0, q.X1);
+            Assert.AreEqual(double.NaN, q.X2);
+        }
+
+        [TestMethod]
+        public void Test_One_Solution_A_Equal_To_Zero()
+        {
+            const double a = 0;
+            const double b = 4;
+            const double c = 4;
+            var q = new Quad.Quad(a, b, c);
+            q.Calculate();
+            Assert.AreEqual(-1.0, q.X1);
+            Assert.AreEqual(double.NaN, q.X2);
+        }
+
+        [TestMethod]
+        public void Test_Two_Solution_B_Equal_To_Zero()
+        {
+            const double a = 5;
+            const double b = 0;
+            const double c = -125;
+            var q = new Quad.Quad(a, b, c);
+            q.Calculate();
+            Assert.AreEqual(5.0, q.X1);
+            Assert.AreEqual(-5.0, q.X2);
+        }
+
+        [TestMethod]
+        public void Test_No_solutions_B_Zero()
+        {
+            const double a = 5;
+            const double b = 0;
+            const double c = 3;
+            var q = new Quad.Quad(a, b, c);
+            q.Calculate();
+            Assert.AreEqual(double.NaN, q.X1);
+            Assert.AreEqual(double.NaN, q.X2);
+        }
+
+        [TestMethod]
+        public void Test_No_solutions_A_Zero_B_Zero()
+        {
+            const double a = 0;
+            const double b = 0;
+            const double c = -30;
+            var q = new Quad.Quad(a, b, c);
+            q.Calculate();
+            Assert.AreEqual(double.NaN, q.X1);
+            Assert.AreEqual(double.NaN, q.X2);
+        }
+
+        [TestMethod]
+        public void Test_No_solutions_All_Zero()
+        {
+            const double a = 0;
+            const double b = 0;
+            const double c = 0;
+            var q = new Quad.Quad(a, b, c);
+            q.Calculate();
+            Assert.AreEqual(double.NaN, q.X1);
+            Assert.AreEqual(double.NaN, q.X2);
+        }
+
+        [TestMethod]
+        public void Test_No_solutions()
+        {
+            const double a = 1;
+            const double b = 2;
+            const double c = 3;
+            var q = new Quad.Quad(a, b, c);
+            q.Calculate();
+            Assert.AreEqual(double.NaN, q.X1);
+            Assert.AreEqual(double.NaN, q.X2);
+        }
+    }
+}
