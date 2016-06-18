882f27fce8993677fbe9e283deafb07539788422
KamilKhair <KamilK@code-college.net>
2016-06-18 21:47:35 +0300

    Working With Strings

---
+++ Lab4.1/Strings/Strings/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Strings
+{
+    public class Sentence
+    {
+        private readonly string _sentense;
+
+        public Sentence(string sentence)
+        {
+            _sentense = sentence;
+        }
+
+        public string[] Arr { get; private set; }
+
+        public void SplitSentence()
+        {
+            Arr = _sentense.Split();
+        }
+
+        public int NumberOfWords()
+        {
+            if (Arr == null)
+            {
+                return -1;
+            }
+            return Arr.Length;
+        }
+
+        public void ReverseWords()
+        {
+            if (Arr == null)
+            {
+                return;
+            }
+            for (var i = 0; i < Arr.Length; ++i)
+            {
+                var charArray = Arr[i].ToCharArray();
+                Array.Reverse(charArray);
+                Arr[i] = new string(charArray);
+            }
+        }
+
+        public void SortSentence()
+        {
+            if (Arr == null)
+            {
+                return;
+            }
+            Array.Sort(Arr);
+        }
+    }
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please insert a sentence of words: (insert empty string to exit)");
+            var s = Console.ReadLine();
+            while (s != "")
+            {
+                if (s != null)
+                {
+                    var sentence = new Sentence(s);
+                    sentence.SplitSentence();
+                    Console.WriteLine("Number of words is: " + sentence.NumberOfWords());
+                    Console.WriteLine();
+                    sentence.ReverseWords();
+                    Console.WriteLine("After Reversing the words:");
+
+                    foreach (var t in sentence.Arr)
+                    {
+                        Console.Write(t + " ");
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    sentence.SortSentence();
+
+                    Console.WriteLine("After Sorting the array:");
+                    foreach (var t in sentence.Arr)
+                    {
+                        Console.Write(t + " ");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Please insert a sentence of words: (insert empty string to exit)");
+                s = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Lab4.1/Strings/StringsUnitTest/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringsUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_SplitSentence()
+        {
+            var sentence = new Sentence("Hello C#!");
+            sentence.SplitSentence();
+            Assert.AreEqual(2, sentence.Arr.Length);
+            Assert.AreEqual("Hello", sentence.Arr[0]);
+            Assert.AreEqual("C#!", sentence.Arr[1]);
+        }
+
+        [TestMethod]
+        public void Test_NumberOfWords()
+        {
+            var sentence = new Sentence("Hello C#!");
+            sentence.SplitSentence();
+            Assert.AreEqual(2, sentence.NumberOfWords());
+        }
+
+        [TestMethod]
+        public void Test_NumberOfWords_Without_Splitting()
+        {
+            var sentence = new Sentence("Hello C#!");
+            Assert.AreEqual(-1, sentence.NumberOfWords());
+        }
+
+        [TestMethod]
+        public void Test_ReverseWords()
+        {
+            var sentence = new Sentence("Hello C#!");
+            sentence.SplitSentence();
+            sentence.ReverseWords();
+            Assert.AreEqual("olleH", sentence.Arr[0]);
+            Assert.AreEqual("!#C", sentence.Arr[1]);
+        }
+
+        [TestMethod]
+        public void Test_ReverseWords_Without_Splitting()
+        {
+            var sentence = new Sentence("Hello C#!");
+            sentence.ReverseWords();
+            Assert.AreEqual(null, sentence.Arr);
+        }
+
+        [TestMethod]
+        public void Test_SortSentence()
+        {
+            var sentence = new Sentence("Hello C#!");
+            sentence.SplitSentence();
+            Assert.AreEqual("Hello", sentence.Arr[0]);
+            Assert.AreEqual("C#!", sentence.Arr[1]);
+            sentence.SortSentence();
+            Assert.AreEqual("C#!", sentence.Arr[0]);
+            Assert.AreEqual("Hello", sentence.Arr[1]);
+        }
+
+        [TestMethod]
+        public void Test_SortSentence_Without_Splitting()
+        {
+            var sentence = new Sentence("Hello C#!");
+            sentence.SortSentence();
+            Assert.AreEqual(null, sentence.Arr);
+        }
+    }
+}
