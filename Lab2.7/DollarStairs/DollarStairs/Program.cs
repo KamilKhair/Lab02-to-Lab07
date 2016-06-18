738f1170b2e6de34150d92816b66fb256f5ea8c5
KamilKhair <KamilK@code-college.net>
2016-06-18 20:31:48 +0300

    Using Loops II

---
+++ Lab2.7/DollarStairs/DollarStairs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DollarStairs
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+            Console.WriteLine("Plaese enter an integer number");
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Plaese enter an integer number");
+            }
+            for (var i = 1; i < n + 1; ++i)
+            {
+                DisplayDollars(i);
+            }
+        }
+
+        internal static void DisplayDollars(int i)
+        {
+            for (var j = 0; j < i; ++j)
+            {
+                Console.Write("$");
+            }
+            Console.WriteLine();
+        }
+    }
+}
