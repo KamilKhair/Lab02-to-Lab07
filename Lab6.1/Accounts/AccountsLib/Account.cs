444ed990acb2a386c022ce227f2441ae0911fdb6
KamilKhair <KamilK@code-college.net>
2016-07-01 11:00:50 +0300

    Lab6.1  Catching and Throwing Exceptions

---
+++ Lab6.1/Accounts/AccountsLib/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace AccountsLib
+{
+    public class Account
+    {
+        internal Account(int id)
+        {
+            Id = id;
+            Balance = 0;
+        }
+
+        public int Id { get; }
+
+        public double Balance { get; private set; }
+
+        public void Deposit(double amount)
+        {
+            if (!(amount > 0))
+            {
+                throw new ArgumentOutOfRangeException(null, "Hey! you can not deposit 0 or negative amount! ");
+            }
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (!(Balance > amount))
+            {
+                throw new ArgumentOutOfRangeException(null ,"Hey! you can not go into overdraft! ");
+            }
+            Balance -= amount;
+        }
+
+        public void Transfer(Account distenationAcc, double amount)
+        {
+            var attempt = true;
+            try
+            {
+                if (!(Balance >= amount) || amount < 0)
+                {
+                    attempt = false;
+                    throw new ArgumentOutOfRangeException(null, "Illigal amount to transfer!");
+                }
+                distenationAcc.Balance += amount;
+                Balance -= amount;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (attempt)
+                {
+                    Console.WriteLine("Transfer has been attempt!");
+                    Console.WriteLine("Account id = " + Id + " has a new balance = " + Balance);
+                    Console.WriteLine("Account id = " + distenationAcc.Id + " has a new balance = " + distenationAcc.Balance);
+                }
+                else
+                {
+                    Console.WriteLine("Transfer has not been attempt!");
+                }
+            }
+        }
+    }
+}
