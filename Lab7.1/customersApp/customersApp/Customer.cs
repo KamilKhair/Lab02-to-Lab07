95fa33dd3a80aafbcb2a800fc243040d316793e5
KamilKhair <KamilK@code-college.net>
2016-07-01 12:48:40 +0300

    Lab 7.1 Generic Interface Implementation

---
+++ Lab7.1/customersApp/customersApp/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customersApp
+{
+    public class Customer : IComparable<Customer>, IEquatable<Customer>
+    {
+        public string Name { get; }
+        public int Id { get; }
+        public string Address { get; }
+
+        public Customer(string name, int id, string address)
+        {
+            Name = name;
+            Id = id;
+            Address = address;
+        }
+
+        public int CompareTo(Customer other)
+        {
+            // If other is not a valid object reference, this instance is greater, else the names are compared.
+            return other == null ? 1 : Name.ToLower().CompareTo(other.Name.ToLower());
+        }
+
+        public bool Equals(Customer other)
+        {
+            if (other == null)
+                return false;
+
+            return Name == other.Name && Id == other.Id;
+        }
+    }
+
+    public class AnotherCustomerComparer : IComparer<Customer>
+    {
+        public int Compare(Customer x, Customer y)
+        {
+            return x.Id.CompareTo(y.Id) != 0 ? x.Id.CompareTo(y.Id) : 0;
+        }
+    }
+}
---
+++ Lab7.1/customersApp/customersApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace customersApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var customersArray = new Customer[6];
+            customersArray[0] = new Customer("Kamil", 301522637, "Maghar");
+            customersArray[1] = new Customer("kamil", 225456874, "Haifa");
+            customersArray[2] = new Customer("Yair", 125445687, "Tel-Aviv");
+            customersArray[3] = new Customer("yair", 125445687, "Haifa");
+            customersArray[4] = new Customer("amir", 125445687, "Tel-Aviv");
+            customersArray[5] = new Customer("Amir", 125445687, "Maghar");
+
+            Displayarray(customersArray);
+
+            Array.Sort(customersArray);
+
+            Displayarray(customersArray);
+
+            var comparer = new AnotherCustomerComparer();
+
+            Array.Sort(customersArray, comparer);
+
+            Displayarray(customersArray);
+        }
+
+        public static void Displayarray(Customer[] arr)
+        {
+            foreach (var customer in arr)
+            {
+                Console.WriteLine($"{customer.Name} {customer.Id} {customer.Address}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
