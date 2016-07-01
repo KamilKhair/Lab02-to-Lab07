b00865e433c5350e39170b98deaf1359ce79ea01
KamilKhair <KamilK@code-college.net>
2016-07-01 11:23:43 +0300

    Lab 6.2: Custom Exceptions

---
+++ Lab6.2/Accounts/AccountsLib/AccountsFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace AccountsLib
+{
+    public static class AccountFactory
+    {
+        public static Account CreateAccount(int id, double balance)
+        {
+            var account = new Account(id);
+            account.Deposit(balance);
+            return account;
+        }
+    }
+}
