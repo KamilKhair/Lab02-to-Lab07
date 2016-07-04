1f2242d8cfcd9047c80c0665e182d3022025ddc9
KamilKhair <KamilK@code-college.net>
2016-07-04 17:31:18 +0300

    Lab7.2 Generic Class

---
+++ Lab7.2/GenericApp/GenericApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GenericApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dictionary = new MultiDictionary<int, string>();
+
+            dictionary.Add(1, "one");
+            dictionary.Add(2, "two");
+            dictionary.Add(3, "three");
+            dictionary.Add(1, "ich");
+            dictionary.Add(2, "nee");
+            dictionary.Add(3, "sun");
+
+
+            foreach (var key in dictionary)
+            {
+                foreach (var value in key.Value)
+                {
+                    Console.WriteLine($"Key = {key.Key}, Value = {value}");
+                }
+            }
+        } 
+    }
+}
