1a9dbc60845af3486cab9bbaef77529bd601a47a
KamilKhair <KamilK@code-college.net>
2016-07-05 15:05:17 +0300

    Lab3.2 Creating Value Types

--- Lab3.2/Rationals/Rationals/Program.cs
+++ Lab3.2/Rationals/Rationals/Program.cs
@@ -2,93 +2,39 @@
 
 namespace Rationals
 {
-    struct Rational
+    internal class Program
     {
-        public Rational(int numerator, int denomirator)
+        internal static void Main()
         {
-            Numrrator = numerator;
-            Denomirator = denomirator == 0 ? 1 : denomirator;
-        }
-
-        public Rational(int numerator)
-        {
-            Numrrator = numerator;
-            Denomirator = 1;
-        }
-        public int Numrrator { get; private set; }
-
-        public int Denomirator { get; private set; }
-
-        public double Value => (double)Numrrator / Denomirator;
-
-        public Rational Add(Rational r)
-        {
-            if (Denomirator == r.Denomirator)
+            var rationalArr = new[]
             {
-                Rational newRational = new Rational(Numrrator + r.Numrrator, Denomirator);
-                return newRational;
-            }
-            else
+                new Rational(2, 12),
+                new Rational(6, 32),
+                new Rational(),
+                new Rational()
+            };
+
+            rationalArr[2] = rationalArr[0].Add(rationalArr[1]);
+            rationalArr[3] = rationalArr[0].Mul(rationalArr[1]);
+
+
+            Console.WriteLine("Before Reduce:");
+            DisplayArr(rationalArr);
+
+            for (var i = 0; i < rationalArr.Length; ++i)
             {
-                int denomirator = Denomirator * r.Denomirator;
-                Rational newRational = new Rational((denomirator / r.Denomirator) * r.Numrrator + (denomirator / Denomirator) * Numrrator, denomirator);
-                return newRational;
+                rationalArr[i].Reduce();
             }
-        }
 
-        public Rational Mul(Rational r)
-        {
-            var newRational = new Rational(Numrrator * r.Numrrator, Denomirator * r.Denomirator);
-            return newRational;
-        }
-
-        public void Reduce()
-        {
-            var numerator = Numrrator;
-            Numrrator /= Gcd(Numrrator, Denomirator);
-            Denomirator /= Gcd(numerator, Denomirator);
-        }
-
-        public override string ToString()
-        {
-            return Numrrator.ToString() + "/" + Denomirator.ToString();
-        }
-
-        public static int Gcd(int a, int b) // Greatest Common Divisor
-        {
-            while (true)
-            {
-                if (b == 0)
-                {
-                    return a;
-                }
-                var r = a % b;
-                a = b;
-                b = r;
-            }
-        }
-    }
-
-    class Program
-    {
-        static void Main()
-        {
-            var r1 = new Rational(2, 12);
-            var r2 = new Rational(6, 32);
-            var r3 = r1.Add(r2);
-            var r4 = r1.Mul(r2);
-
-            Console.WriteLine(r1.ToString() + " + " + r2.ToString() + " = " + r3.ToString() + " = " + r3.Value);
-            Console.WriteLine(r1.ToString() + " * " + r2.ToString() + " = " + r4.ToString() + " = " + r4.Value);
-
-            r1.Reduce();
-            r2.Reduce();
-            r3.Reduce();
-            r4.Reduce();
-
+            Console.WriteLine();
             Console.WriteLine("After Reduce:");
-            Console.WriteLine(r1.ToString() + " + " + r2.ToString() + " = " + r3.ToString() + " = " + r3.Value);
-            Console.WriteLine(r1.ToString() + " * " + r2.ToString() + " = " + r4.ToString() + " = " + r4.Value);
+            DisplayArr(rationalArr);
+        }
+
+        private static void DisplayArr(Rational[] rationalArr)
+        {
+            Console.WriteLine(rationalArr[0].ToString() + " + " + rationalArr[1].ToString() + " = " + rationalArr[2].ToString() + " = " + rationalArr[2].Value);
+            Console.WriteLine(rationalArr[0].ToString() + " * " + rationalArr[1].ToString() + " = " + rationalArr[3].ToString() + " = " + rationalArr[3].Value);
         }
     }
 }
---
+++ Lab3.2/Rationals/Rationals/Rational.cs
@@ -0,0 +1,69 @@
+﻿namespace Rationals
+{
+    internal struct Rational
+    {
+        public Rational(int numerator, int denomirator)
+        {
+            Numrrator = numerator;
+            Denomirator = denomirator == 0 ? 1 : denomirator;
+        }
+
+        public Rational(int numerator)
+        {
+            Numrrator = numerator;
+            Denomirator = 1;
+        }
+        public int Numrrator { get; private set; }
+
+        public int Denomirator { get; private set; }
+
+        public double Value => (double)Numrrator / Denomirator;
+
+        public Rational Add(Rational r)
+        {
+            if (Denomirator == r.Denomirator)
+            {
+                var newRational = new Rational(Numrrator + r.Numrrator, Denomirator);
+                return newRational;
+            }
+            else
+            {
+                var denomirator = Denomirator * r.Denomirator;
+                var newRational = new Rational((denomirator / r.Denomirator) * r.Numrrator + (denomirator / Denomirator) * Numrrator, denomirator);
+                return newRational;
+            }
+        }
+
+        public Rational Mul(Rational r)
+        {
+            var newRational = new Rational(Numrrator * r.Numrrator, Denomirator * r.Denomirator);
+            return newRational;
+        }
+
+        public void Reduce()
+        {
+            var numerator = Numrrator;
+            Numrrator /= Gcd(Numrrator, Denomirator);
+            Denomirator /= Gcd(numerator, Denomirator);
+        }
+
+        public override string ToString()
+        {
+            return Numrrator.ToString() + "/" + Denomirator.ToString();
+        }
+
+        public static int Gcd(int a, int b) // Greatest Common Divisor
+        {
+            while (true)
+            {
+                if (b == 0)
+                {
+                    return a;
+                }
+                var r = a % b;
+                a = b;
+                b = r;
+            }
+        }
+    }
+}
